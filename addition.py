4d9b0c3879ce781ed2de8c3a0717d9adb25db1d4
Mr. R <31701130+sweetcbk@users.noreply.github.com>
2022-11-09 15:06:08 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,20 @@
+
+# Addition
+#-------------------------------------------
+a = int(input("Enter First number: "))
+b = int(input("Enter Second number: "))
+
+sum = int(a) + int(b)
+print("Sum : ", sum)
+
+
+# Squared 2
+#---------------------------------------------
+squared= a*a
+print("Squared is : ",squared)
+
+
+# Raised
+#-----------------------------------------------
+rise= a*b
+print("Number Raiseed : ",rise)
